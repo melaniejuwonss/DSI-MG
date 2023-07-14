a400c3eec1701498f0e2c972141398fe399dc680
melanie <yjw9700@agape.hanyang.ac.kr>
2023-07-14 22:35:51 +0900

    2023.07.14: test dataset gold 로 변경

--- train.py
+++ train.py
@@ -249,7 +249,7 @@ def main(args):
     training_args = TrainingArguments(
         output_dir="./results",
         learning_rate=args.learning_rate,  # 0.0005,
-        warmup_steps=10000,
+        # warmup_steps=10000,
         # weight_decay=0.01,
         per_device_train_batch_size=args.train_batch_size,
         per_device_eval_batch_size=args.eval_batch_size,
